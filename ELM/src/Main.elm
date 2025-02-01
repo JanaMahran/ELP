014d7265df985d386c786236f53dce9d8443c7a0
JanaMahran <jana.mahran@insa-lyon.fr>
2025-02-01 16:35:05 +0100

    A little less ugly HTML page...
    updated view function pour rendre le page html un peu moins moche

--- ELM/src/Main.elm
+++ ELM/src/Main.elm
@@ -53,16 +53,49 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ input
-            [ placeholder "Enter your TcTurtle program here"
-            , value model.userInput
-            , onInput UpdateInput
+    div [ Html.Attributes.style "text-align" "center" ]
+        [ Html.h1 [ Html.Attributes.style "font-size" "2em", Html.Attributes.style "color" "#333" ] [ text "Welcome to TcTurtle!" ]
+        , Html.p [ Html.Attributes.style "font-size" "1.2em", Html.Attributes.style "color" "#666", Html.Attributes.style "margin-bottom" "20px" ] [ text "Start by writing the following instruction! xx" ]
+        , div [ Html.Attributes.style "margin" "auto", Html.Attributes.style "width" "80%", Html.Attributes.style "display" "flex", Html.Attributes.style "justify-content" "space-between" ]
+            [ div [ Html.Attributes.style "width" "65%", Html.Attributes.style "text-align" "left" ]
+                [ input
+                    [ placeholder "Enter your TcTurtle program here"
+                    , value model.userInput
+                    , onInput UpdateInput
+                    , Html.Attributes.style "width" "100%"
+                    ]
+                    []
+                , button
+                    [ onClick ParseInput
+                    , Html.Attributes.style "background-color" "grey"
+                    , Html.Attributes.style "color" "white"
+                    , Html.Attributes.style "font-size" "1em"
+                    , Html.Attributes.style "padding" "10px 20px"
+                    , Html.Attributes.style "border" "none"
+                    , Html.Attributes.style "border-radius" "5px"
+                    , Html.Attributes.style "width" "100%"
+                    , Html.Attributes.style "margin-top" "10px"
+                    ]
+                    [ text "Parse and Draw" ]
+                , div [ Html.Attributes.style "margin-top" "20px" ]
+                    [ viewOutput model.parsedInstructions ]
+                ]
+            , div [ Html.Attributes.style "width" "30%", Html.Attributes.style "background-color" "grey", Html.Attributes.style "padding" "10px", Html.Attributes.style "border-radius" "5px", Html.Attributes.style "color" "white" ]
+                [ Html.h2 [ Html.Attributes.style "font-size" "1.5em", Html.Attributes.style "color" "white" ] [ text "TcTurtle Examples" ]
+                , Html.ul []
+                    [ Html.li [] [ text "To draw a square:[ Repeat 4 [ Forward 100 Left 90 ] ]" ]
+                    , Html.li [] [ text "To draw a star:[ Repeat 5 [ Forward 100, Right 144 ] ]" ]
+                    , Html.li [] [ text "To draw a circle:[ Repeat 36 [ Forward 10, Right 10 ] ]" ]
+                    , Html.li [] [ text "To draw a triangle:[ Repeat 3 [ Forward 100, Left 120 ] ]" ]
+                    , Html.li [] [ text "To draw a hexagon:[ Repeat 6 [ Forward 100, Left 60 ] ]" ]
+                    , Html.li [] [ text "To draw a spiral:[ Repeat 36 [ Forward 10, Right 10 ] ]" ]
+                    ]
+                ]
             ]
-            []
-        , button [ onClick ParseInput ] [ text "Parse and Draw" ]
-        , div [] [ viewOutput model.parsedInstructions ]
         ]
+
+
+
 
 
 viewOutput : Result String (List Instruction) -> Html msg
@@ -73,7 +106,7 @@ viewOutput result =
                 [ drawInstructions instructions ]
 
         Err error ->
-            text ("Error: " ++ error)
+            text (error)
 
 
 -- MAIN
